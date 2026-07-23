c7bdfd36f2834a18f01244ce5ad86cfaa458f2db
Zuk4r1 <babiloniaetica@gmail.com>
2026-07-22 21:22:13 -0500

    Update Aplicación.tsx

--- src/Aplicación.tsx
+++ src/Aplicación.tsx
@@ -80,7 +80,7 @@ export default function Aplicación() {
               <Sidebar />
 
               {/* Contenido Principal */}
-              <main className="flex-1 px-4 md:px-8 py-6 md:py-8 ml-0 md:ml-64 min-h-[calc(100vh-80px)] transition-all duration-300 gpu-smooth">
+              <main className="flex-1 min-w-0 px-4 md:px-8 py-6 md:py-8 ml-0 md:ml-64 min-h-[calc(100vh-80px)] transition-all duration-300 gpu-smooth">
                 <div className="max-w-7xl mx-auto animate-fade-in">
                   <Routes />
                 </div>
