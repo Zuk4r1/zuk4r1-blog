74e71004d55002e79a56d860989f53fd1a20783c
Zuk4r1 <investigacion1956@gmail.com>
2026-08-22 14:29:14 -0500

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from './lib/query-client';
 import Aplicación from './Aplicación';
 import './index.css';
 import './App.css';
@@ -9,8 +7,6 @@ import './App.css';
 // Renderizar la aplicación
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Aplicación />
-    </QueryClientProvider>
+    <Aplicación />
   </React.StrictMode>
 );
